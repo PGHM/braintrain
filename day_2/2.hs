df849f23ccc5eb6318d44a52363e4253088de256
Jussi Päivinen <jussi.paivinen@houston-inc.com>
2017-05-10 14:58:58 +0300

    Solved day 2 part 1

---
+++ day_2/2.hs
@@ -0,0 +1,19 @@
+import System.IO
+import Data.List
+
+main = do
+    contents <- readFile "2.txt"
+    let instructions = lines contents
+    let codeMaker = (\ s@(x:xs) y -> solveInstruction y x : s)
+    let code = show . drop 1 . reverse $ foldl' codeMaker [5] instructions
+    print ("The code to the bathroom is: " ++ code)
+
+solveInstruction :: String -> Int -> Int
+solveInstruction (x:"") y = nextNumber x y
+solveInstruction (x:xs) y = solveInstruction xs $ nextNumber x y
+
+nextNumber :: Char -> Int -> Int
+nextNumber 'U' x = if x - 3 < 1 then x else x - 3
+nextNumber 'D' x = if x + 3 > 9 then x else x + 3
+nextNumber 'L' x = if x `mod` 3 == 1 then x else x - 1
+nextNumber 'R' x = if x `mod` 3 == 0 then x else x + 1
