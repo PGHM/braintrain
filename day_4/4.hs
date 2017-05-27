d4acfa683225292e6691b5dd518da1eadd5a2a6e
Jussi Päivinen <jussi.paivinen@houston-inc.com>
2017-05-27 14:24:14 +0300

    Solution to day 4

---
+++ day_4/4.hs
@@ -0,0 +1,30 @@
+import System.IO
+import Data.List
+import Data.Char
+import Data.Ord
+import Data.Function
+
+main = do
+    let notNumber = not . isDigit
+    let readInt = read :: String -> Int
+
+    contents <- readFile "4.txt"
+    let rooms = lines $ contents
+    let names = map (filter (/='-') . takeWhile notNumber) rooms
+    let sectorIDs = map (readInt . take 3 . dropWhile notNumber) rooms
+    let checksums = map (take 5 . drop 1 . dropWhile (/='[')) rooms
+    let sectorSum = sum $ map roomValue $ zip3 names sectorIDs checksums
+    print ("Sum of the real room sector IDs is: " ++ sectorSum)
+
+roomValue :: (String, Int, String) -> Int
+roomValue (name, sectorID, checksum) = if realRoom name checksum then sectorID else 0
+
+realRoom :: String -> String -> Bool
+realRoom name checksum = fiveMostCommon == checksum
+    where 
+        uniqueLetters = sort . nub $ name
+        occurances = map (\x -> length $ filter (==x) name) uniqueLetters
+        fiveMostCommon = snd . unzip . take 5 . sortDescOnFirst $ zip occurances uniqueLetters
+
+sortDescOnFirst :: Ord a => [(a, b)] -> [(a, b)]
+sortDescOnFirst = sortBy (flip compare `on` fst)
---
+++ day_4/4_2.hs
@@ -0,0 +1,40 @@
+import System.IO
+import Data.List
+import Data.Char
+import Data.Ord
+import Data.Function
+import Data.Maybe
+
+main = do
+    let notNumber = not . isDigit
+    let readInt = read :: String -> Int
+
+    contents <- readFile "4.txt"
+    let rooms = lines $ contents
+    let names = map (filter (/='-') . takeWhile notNumber) rooms
+    let sectorIDs = map (readInt . take 3 . dropWhile notNumber) rooms
+    let checksums = map (take 5 . drop 1 . dropWhile (/='[')) rooms
+    let realRooms = filter realRoom $ zip3 names sectorIDs checksums
+    let decryptedRooms = map decryptedRoom realRooms
+    let northPoleObjectsRoom = fromJust . find ((=="northpoleobjectstorage") . fst) $ decryptedRooms
+    print ("North pole object storage room is in sector: " ++ (show . snd $ northPoleObjectsRoom))
+
+realRoom :: (String, Int, String) -> Bool
+realRoom (name, _, checksum) = fiveMostCommon == checksum
+    where 
+        uniqueLetters = sort . nub $ name
+        occurances = map (\x -> length $ filter (==x) name) uniqueLetters
+        fiveMostCommon = snd . unzip . take 5 . sortDescOnFirst $ zip occurances uniqueLetters
+
+decryptedRoom :: (String, Int, String) -> (String, Int)
+decryptedRoom (name, sectorID, _) = (decryptedName, sectorID)
+    where
+        decryptedName = map (\x -> iterate shiftCipher x !! sectorID) name
+
+sortDescOnFirst :: Ord a => [(a, b)] -> [(a, b)]
+sortDescOnFirst = sortBy (flip compare `on` fst)
+
+shiftCipher :: Char -> Char
+shiftCipher x
+    | x == 'z' = 'a'
+    | otherwise = succ x
