01a4a5a17aaf5d90201a2613b2d8017487d7251a
Jussi Päivinen <jussi.paivinen@houston-inc.com>
2018-02-23 16:05:38 +0200

    Albums project ready

---
+++ react_native/albums/src/components/AlbumDetail.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image, url } = album;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image resizeMode='contain' style={styles.imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)} >Buy now!</Button>
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+
+  imageStyle: {
+    flex: 1,
+    height: 300,
+    width: null,
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  headerTextStyle: {
+    fontSize: 18
+  }
+};
+
+export default AlbumDetail;
