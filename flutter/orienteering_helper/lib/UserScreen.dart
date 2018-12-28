65bc1c440afa2727437fbe8ca1affd00b41c918a
Jussi Päivinen <jussi.paivinen@houston-inc.com>
2018-12-28 15:53:36 +0200

    1.5 days with Flutter, using Firebase for something and making a list

--- flutter/orienteering_helper/lib/UserScreen.dart
+++ flutter/orienteering_helper/lib/UserScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
 class UserScreen extends StatefulWidget {
   @override
@@ -6,18 +8,36 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  final _users = <String>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getTracks();
+  }
+
+  void _getTracks() async {
+    final snapshot = await Firestore.instance.collection('tracks').getDocuments();
+    setState(() {
+      _users.clear();
+      _users.addAll(
+          snapshot.documents.map((document) => document.data['name']));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Orienteering Helper"),
       ),
-      body: ListView.builder(itemBuilder: (context, i) {
-        return Center(child: Text("Something related to orienteering"));
-      }),
+      body: ListView(
+        children: _users.map((username) => Text(username)).toList(),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
+        onPressed: _getTracks,
+        child: Icon(Icons.refresh),
       ),
     );
   }
