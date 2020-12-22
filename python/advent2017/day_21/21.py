0c65fe9184fe8515889117ebbeacceb6518da56d
Jussi Päivinen <jussi.paivinen@gmail.com>
2020-12-22 14:14:40 +0200

    Improved readability of Day 21

--- python/advent2017/day_21/21.py
+++ python/advent2017/day_21/21.py
@@ -9,7 +9,7 @@ Rule = namedtuple('Rule', 'inputs output')
 # Parsing
 
 def parse_rule(rule_string):
-    parts = [x.strip() for x in  rule_string.split("=>")]
+    parts = [part.strip() for part in rule_string.split("=>")]
     rule_output = parts[1].split("/")
     
     rule_default_input = parts[0].split("/")
@@ -43,25 +43,33 @@ def transpose(grid, result):
     if len(grid[0]) == 0:
         return result
 
-    row_from_column = ''.join([x[:1] for x in grid])
-    column_stripped_grid = [x[1:] for x in grid]
+    row_from_column = ''.join([row[:1] for row in grid])
+    column_stripped_grid = [row[1:] for row in grid]
     return transpose(column_stripped_grid, result + [row_from_column])
 
 # Dividing
 
-def divide_grid(grid, divisor, sub_grids):
+def divide_grid(grid):
+    if len(grid[0]) % 2 == 0:
+        divisor = 2
+    else:
+        divisor = 3
+
+    return _divide_grid(grid, divisor, [])
+
+def _divide_grid(grid, divisor, sub_grids):
     if len(grid) == 0:
         return sub_grids
 
-    sub_grids += divide_divisor_height_grid(grid[:divisor], divisor, [])
-    return divide_grid(grid[divisor:], divisor, sub_grids)
+    new_sub_grids = divide_divisor_height_grid(grid[:divisor], divisor, [])
+    return _divide_grid(grid[divisor:], divisor, sub_grids + new_sub_grids)
 
 def divide_divisor_height_grid(grid, divisor, sub_grids):
     if len(grid[0]) == 0:
         return sub_grids
 
-    new_sub_grid = [x[:divisor] for x in grid]
-    stripped_grid = [x[divisor:] for x in grid]
+    new_sub_grid = [row[:divisor] for row in grid]
+    stripped_grid = [row[divisor:] for row in grid]
     return divide_divisor_height_grid(
         stripped_grid, 
         divisor, 
@@ -74,27 +82,29 @@ def combine_sub_grids(sub_grids):
     sub_grids_per_row = int(math.sqrt(len(sub_grids)))
     return _combine_sub_grids(sub_grids, sub_grids_per_row, [])
 
-def _combine_sub_grids(sub_grids, sub_grids_per_row, result):
+def _combine_sub_grids(sub_grids, sub_grids_per_row, grid):
     if len(sub_grids) == 0:
-        return result
+        return grid
 
     row_of_sub_grids = sub_grids[:sub_grids_per_row]
-    result += combine_sub_grid_row(row_of_sub_grids, [])
+    new_grid_rows = combine_row_of_sub_grids(row_of_sub_grids, [])
     return _combine_sub_grids(
         sub_grids[sub_grids_per_row:],
         sub_grids_per_row, 
-        result
+        grid + new_grid_rows
     )
 
-def combine_sub_grid_row(row_of_sub_grids, result):
-    if len(row_of_sub_grids[0]) == 0:
-        return result
+def combine_row_of_sub_grids(sub_grids, new_grid_rows):
+    if len(sub_grids[0]) == 0:
+        return new_grid_rows
     
-    first_rows = [x[0] for x in row_of_sub_grids]
-    combined_first_rows = ''.join(first_rows)
-    result += [combined_first_rows]
-    stripped_row_of_sub_grids = [x[1:] for x in row_of_sub_grids]
-    return combine_sub_grid_row(stripped_row_of_sub_grids, result)
+    first_rows_from_sub_grids = [x[0] for x in sub_grids]
+    new_grid_row = ''.join(first_rows_from_sub_grids)
+    sub_grids_without_first_row = [x[1:] for x in sub_grids]
+    return combine_row_of_sub_grids(
+        sub_grids_without_first_row, 
+        new_grid_rows + [new_grid_row]
+    )
 
 # Expanding
 
@@ -102,13 +112,8 @@ def expand_grid(grid, iterations_left, rules):
     print(iterations_left)
     if iterations_left == 0:
         return grid
-
-    if len(grid[0]) % 2 == 0:
-        divisor = 2
-    else:
-        divisor = 3
     
-    sub_grids = divide_grid(grid, divisor, [])
+    sub_grids = divide_grid(grid)
     expanded_sub_grids = [expand_sub_grid(x, rules) for x in sub_grids]
     new_grid = combine_sub_grids(expanded_sub_grids)
     return expand_grid(new_grid, iterations_left - 1, rules)
@@ -137,7 +142,7 @@ grid = [
 first_part_iterations = 5
 second_part_extra_iterations = 13
 
-rules = [parse_rule(x.rstrip()) for x in open('input', 'r').readlines()]
+rules = [parse_rule(line.strip()) for line in open('input', 'r').readlines()]
 first_part_final_grid = expand_grid(grid, first_part_iterations, rules)
 second_part_final_grid = expand_grid(
     first_part_final_grid,
