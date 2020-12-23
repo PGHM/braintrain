829bcd0d422f508347b0b17cb4ef86ff8ce78167
Jussi Päivinen <jussi.paivinen@gmail.com>
2020-12-23 09:46:02 +0200

    Optimizing solution run time

--- python/advent2017/day_22/22_2.py
+++ python/advent2017/day_22/22_2.py
@@ -1,100 +1,79 @@
-from collections import namedtuple
-from enum import Enum
+from collections import defaultdict
 
-Movement = namedtuple('Movement', 'diff_x diff_y')
-Virus = namedtuple('Virus', 'node direction')
-Node = namedtuple("Node", 'x y status')
+# Because the approach taken in part 1 did not work at all because it was far
+# too inefficient and I could not figure out a functional way because of
+# Python's limitations this became a lesson in Python's cProfiler and making
+# things efficient instead of beautiful
 
-class NodeStatus(Enum):
-    CLEAN = '.'
-    WEAKENED = 'W'
-    INFECTED = '#'
-    FLAGGED = 'F'
+# Enums are slow, list index lookups (even in very small lists) are slow, len
+# is slow (even in very small lists), making things is slow (obviously)
 
-class TurnDirection(Enum):
-    STRAIGHT = 0
-    LEFT = -1
-    RIGHT = 1
-    REVERSE = 2
+# First run with the approach like in part 1 took 30 minutes (MacBook Pro
+# 2020), this version takes 7.5 seconds (1.9 seconds with PyPy)
 
-class CardinalDirection(Enum):
-    NORTH = Movement(0, 1)
-    EAST = Movement(1, 0)
-    SOUTH = Movement(0, -1)
-    WEST = Movement(-1, 0)
+CLEAN = '.'
+WEAKENED = 'W'
+INFECTED = '#'
+FLAGGED = 'F'
 
-    def by_turning_to(self, turn_direction):
-        all_directions = list(CardinalDirection)
-        current_index = all_directions.index(self)
-        index_after_turning = current_index + turn_direction.value
-        return all_directions[index_after_turning % len(all_directions)]
+# First value is movement in x direction, second in y direction
+NORTH = (0, 1)
+EAST = (1, 0)
+SOUTH = (0, -1)
+WEST = (-1, 0)
 
-def parse_infected_nodes(initial_grid, y, infected_nodes):
+LEFT_DIRECTIONS = {
+    NORTH: WEST,
+    EAST: NORTH,
+    SOUTH: EAST,
+    WEST: SOUTH
+}
+
+RIGHT_DIRECTIONS = {
+    NORTH: EAST,
+    EAST: SOUTH,
+    SOUTH: WEST,
+    WEST: NORTH
+}
+
+def parse_infected_nodes(initial_grid, y, node_statuses):
     if len(initial_grid) == 0:
-        return infected_nodes
+        return
 
     row = enumerate(initial_grid[0])
     for node in row:
-        if node[1]  == '#':
-            infected_nodes[(node[0], y)] = Node(node[0], y, NodeStatus.INFECTED)
+        if node[1] == INFECTED:
+            node_statuses[(node[0], y)] = INFECTED
     
-    return parse_infected_nodes(
-        initial_grid[1:], 
-        y - 1, 
-        infected_nodes
-    )
-
-def burst(virus, unclean_nodes, infection_count):
-    current_x = virus.node.x
-    current_y = virus.node.y
-    unclean_node = unclean_nodes.get((current_x, current_y), None)
-    if unclean_node == None:
-        turn_direction = TurnDirection.LEFT
-        weakened_node = Node(current_x, current_y, NodeStatus.WEAKENED)
-        unclean_nodes[(current_x, current_y)] = weakened_node
-        node_infected = False
-    else:
-        if unclean_node.status == NodeStatus.WEAKENED:
-            turn_direction = TurnDirection.STRAIGHT
-            infected_node = Node(current_x, current_y, NodeStatus.INFECTED)
-            unclean_nodes[(current_x, current_y)] = infected_node
-            node_infected = True
-        elif unclean_node.status == NodeStatus.INFECTED:
-            turn_direction = TurnDirection.RIGHT
-            flagged_node = Node(current_x, current_y, NodeStatus.FLAGGED)
-            unclean_nodes[(current_x, current_y)] = flagged_node
-            node_infected = False
-        elif unclean_node.status == NodeStatus.FLAGGED: 
-            turn_direction = TurnDirection.REVERSE
-            unclean_nodes[(current_x, current_y)] = None
-            node_infected = False
-
-    new_cardinal_direction = virus.direction.by_turning_to(turn_direction)
-    new_x = current_x + new_cardinal_direction.value.diff_x
-    new_y = current_y + new_cardinal_direction.value.diff_y
-    return (
-        Virus(Node(new_x, new_y, None), new_cardinal_direction),
-        unclean_nodes,
-        infection_count + 1 if node_infected else infection_count
-    )
+    return parse_infected_nodes(initial_grid[1:], y - 1, node_statuses)
 
 initial_grid = [line.strip() for line in open('input', 'r').readlines()]
-unclean_nodes = parse_infected_nodes(initial_grid, 0, {})
+node_statuses = defaultdict(lambda: CLEAN)
+parse_infected_nodes(initial_grid, 0, node_statuses)
+
 bursts = 10000000
-start_x = len(initial_grid) // 2
-start_y = -(len(initial_grid) // 2)
-start_node = Node(start_x, start_y, None)
-virus = Virus(start_node, CardinalDirection.NORTH)
+x = len(initial_grid) // 2
+y = -(len(initial_grid) // 2)
+direction = NORTH
 infection_count = 0
 
 for i in range(0, bursts):
-    if i % 10000 == 0:
-        print(i)
+    current_location = (x, y)
+    node_status = node_statuses[current_location]
+    if node_status == CLEAN:
+        direction = LEFT_DIRECTIONS[direction]
+        node_statuses[current_location] = WEAKENED
+    elif node_status == WEAKENED:
+        node_statuses[current_location] = INFECTED
+        infection_count += 1
+    elif node_status == INFECTED:
+        direction = RIGHT_DIRECTIONS[direction]
+        node_statuses[current_location] = FLAGGED
+    elif node_status == FLAGGED: 
+        direction = RIGHT_DIRECTIONS[RIGHT_DIRECTIONS[direction]]
+        node_statuses[current_location] = CLEAN
 
-    virus, unclean_nodes, infection_count = burst(
-        virus, 
-        unclean_nodes,
-        infection_count
-    )
+    x += direction[0]
+    y += direction[1]
 
 print("{} nodes were infected because of new bursts".format(infection_count))
